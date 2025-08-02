0c49c0afb574be9113d754ef74c9d89e527ce4e2
Vipin Bhagat <108531710+vipinbhagat123@users.noreply.github.com>
2025-08-03 00:30:01 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,15 @@
+# palindrome
+
+def k(s):
+    left=0
+    right=len(s)-1
+    while left<right:
+        if s[left]!=s[right]:
+            return False
+        left+=1
+        right-=1
+    return True
+
+
+s="radar"
+print(k(s))
